554c5b60bcfebb65ccad933d8bcc55953d056286
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-09-14 09:02:37 +0530

    Add skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header'
 import Content from './components/Content/Content'
-import Projects from './components/Projects/Projects';
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
 import Footer from './components/Footer/Footer'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Content/>
       <Projects/>
+      <Skills/>
       <Footer/>
     </div>
   );
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills = () => {
+  return (
+   <section className="s-wrapper">
+    <div className="paddings innerWidth s-container">
+        <div className="s-title primaryText">
+            Skills
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Skills
